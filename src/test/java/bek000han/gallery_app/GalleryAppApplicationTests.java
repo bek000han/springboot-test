aa94cd16cdb04a980262c757a4354332cc00e7d3
bek000han <bekuniversity@gmail.com>
2024-04-12 18:04:08 +0100

    project init

---
+++ src/test/java/bek000han/gallery_app/GalleryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bek000han.gallery_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
